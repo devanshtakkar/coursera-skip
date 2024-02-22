d053aace918e6dde82c9dd0ef2104318168650a6
Devansh Takkar <devansh@takkar.ooo>
2024-02-21 21:36:03 -0800

    config changed

--- config_example.py
+++ config_example.py
@@ -1,4 +1,3 @@
-# https://github.com/serv0id/skipera
 
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
